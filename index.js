0609257b0555ad6a5381676fba30cc090df2b8d3
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2021-07-24 19:47:51 -0300

    ejercicio js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hola")
+const botonTortuga = document.getElementById("boton-uno")
+const botonDelfin = document.getElementById("boton-dos")
+const botonPulpo = document.getElementById("boton-tres")
+const pregunta = document.getElementById("span-pregunta")
+const respuesta = document.getElementById("span-respuesta")
+
+
+const respuestaCorrecta = () => {
+    respuesta.textContent= "¡Respuesta correcta!"
+    botonDelfin.style.backgroundColor = "green"
+} 
+
+const respuestaIncorrecta = () => {
+    respuesta.textContent= "¡Respuesta equivocada!"
+    botonDelfin.style.backgroundColor = "green"
+    botonTortuga.style.backgroundColor = "red"
+    botonPulpo.style.backgroundColor = "red"
+}
+
+
+botonDelfin.onclick = respuestaCorrecta
+botonTortuga.onclick = respuestaIncorrecta
+botonPulpo.onclick = respuestaIncorrecta
